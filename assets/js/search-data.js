3f27a2c5a74a544786034b0baa24c6109da98f2b
nithin-portfolio <35574008+nithin-portfolio@users.noreply.github.com>
2026-04-27 23:47:58 +0000

    Deploying to gh-pages from @ nithin-portfolio/dev-blog@cc49d56e25cdee18a7277b7faa60d99b15733aff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,18 +443,18 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("https://www.linkedin.com/in/pvnithinreddy", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%76%65%6E%6B%61%74.%6E%69%74%68%69%6E%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -462,13 +462,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
