8e8db1fee81af08de3a6b8c41fa14959cf525f57
nithin-portfolio <35574008+nithin-portfolio@users.noreply.github.com>
2026-04-28 03:32:01 +0000

    Deploying to gh-pages from @ nithin-portfolio/dev-blog@b13efda873f949bd05e528bada4b509e5251b978 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,7 +19,7 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "nithin-portfolio/dev-blog",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDOSNTz1A",
     "data-category": "Comments",
     "data-category-id": "",
     "data-mapping": "title",
